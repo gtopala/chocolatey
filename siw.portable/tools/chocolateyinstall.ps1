b8c233464ba6ca4167e12ece1af13d19f88bc6cf
Gabriel Topala <gtopala@outlook.com>
2017-04-23 11:58:37 -0400

    SIW 2017 v7.2.0423

---
+++ siw.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'siw.portable'
+$url = 'https://www.gtopala.net/download/siw_tech.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url64 = 'https://www.gtopala.net/download/siw_tech.zip'
+$checksum      = '049A4FD717297C4FAD4B673681572AB4'
+$checksumType  = 'md5'
+ 
+Install-ChocolateyZipPackage $packageName $url $unzipLocation $url64 -Checksum $checksum -ChecksumType $checksumType
+
---
+++ siw/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'siw'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.gtopala.net/download/siw-setup.exe'
+$url64      = 'https://www.gtopala.net/download/siw-setup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'siw*'
+  checksum      = '1A140CE545BFC8AAA2D5E4E35DCE6653'
+  checksumType  = 'md5'
+  checksum64    = '1A140CE545BFC8AAA2D5E4E35DCE6653'
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
