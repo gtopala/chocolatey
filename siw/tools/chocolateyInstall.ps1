ad1b57dbd509433f1fb09088cd43629cba329b4d
Gabriel Topala <gtopala@outlook.com>
2020-05-28 07:07:15 -0400

    SIW 2020 v10.2.0322h

--- siw/tools/chocolateyInstall.ps1
+++ siw/tools/chocolateyInstall.ps1
@@ -18,9 +18,9 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'siw*'
-  checksum      = '378B981DE719182A63EA708EC3D0C0AD'
+  checksum      = 'AA764BAA8F8A450A256EDE8A5ADF20EB'
   checksumType  = 'md5'
-  checksum64    = '378B981DE719182A63EA708EC3D0C0AD'
+  checksum64    = 'AA764BAA8F8A450A256EDE8A5ADF20EB'
   checksumType64= 'md5'
 }
 
